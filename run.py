48f49ed74c9833ff6425dcfc3ff1c2db81570d97
Angel Roy Bernal <angelroybernal@gmail.com>
2020-03-24 22:22:32 -0500

    fixed port binds

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
+_port = os.environ.get('PORT')
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=_port)
