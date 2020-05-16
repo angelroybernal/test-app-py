8953f544bcec4f7c8701f71bf7b404dda6769530
Angel Roy Bernal <angelroybernal@gmail.com>
2020-05-15 21:40:14 -0500

    + Environment name variable

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 _port = os.environ.get('PORT', 5000)
+_username = os.environ.get('MYAPP_USERNAME', 'World')
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return jsonify('Hello %s!' % _username)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=_port)
+    app.run(host='0.0.0.0', port=_port, debug=True)
